22b3d76575dd771379b995ed253af313ae2d1ad7
Amirreza Nasserjafari <amir.nasserjafari@gmail.com>
2025-01-21 15:40:45 +0100

    sachen hinzugefügt

--- TodoAppMaui/TodoAppMaui/MainPage.xaml.cs
+++ TodoAppMaui/TodoAppMaui/MainPage.xaml.cs
@@ -9,7 +9,13 @@
             InitializeComponent();
         }
 
-        
+        private void ChangeThemeClicked(object? sender, EventArgs e)
+        {
+            if (Application.Current.UserAppTheme == AppTheme.Light)
+                Application.Current.UserAppTheme = AppTheme.Dark;
+            else
+                Application.Current.UserAppTheme = AppTheme.Light;
+        }
     }
 
 }
--- TodoAppMaui/TodoAppMaui/MauiProgram.cs
+++ TodoAppMaui/TodoAppMaui/MauiProgram.cs
@@ -13,6 +13,7 @@ namespace TodoAppMaui
                 {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                     fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                    fonts.AddFont("Farsan-Regular.ttf", "farsan");
                 });
 
 #if DEBUG
