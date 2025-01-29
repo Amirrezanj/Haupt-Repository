3f4e47635ea03b84b5132b076a2085c781be6cd6
Amirreza Nasserjafari <amir.nasserjafari@gmail.com>
2025-01-29 15:42:59 +0100

    sachen hinzugefügt

---
+++ TodoAppMaui/TodoAppMaui/Services/PreferenceService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TodoAppMaui.Abstractions;
+
+namespace TodoAppMaui.Services
+{
+    internal class PreferenceService : IpreferenceService
+    {
+        public string AppTheme
+        {
+            get => Preferences.Default.Get("AppTheme", "Light");
+            set => Preferences.Default.Set("AppTheme",value);
+        }
+    }
+}
